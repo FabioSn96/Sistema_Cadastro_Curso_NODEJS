ceeade578e62f4147f8246a4617455b5ce31a770
FabioSn96 <63240489+FabioSn96@users.noreply.github.com>
2022-08-24 17:37:05 -0300

    Add files via upload

---
+++ models/Usuario.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const Usuario = new Schema({
+    cpf: {
+        type: Number,
+        required: true
+    },
+    nome: {
+        type: String,
+        required: true
+    },
+    idade: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Date,
+        default: Date.now()
+    },
+})
+
+mongoose.model("usuarios", Usuario)
